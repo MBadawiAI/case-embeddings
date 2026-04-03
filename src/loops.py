b5e47bc32c4a50d5dc73fbdea7b7feb895c2e0c5
achentouf <achentouf@trn77.phx15.castle.fm>
2026-04-03 23:47:36 +0000

    supporting loss functions in train and eval loopps

--- src/loops.py
+++ src/loops.py
@@ -1,19 +1,29 @@
 import torch
 from tqdm import tqdm
 
-def train_one_epoch(model, loader, optimizer, scheduler, device, epoch, writer, cfg):
+def train_one_epoch(model, loader, optimizer, scheduler, device, epoch, writer, cfg, criterion = None):
+    if criterion is None:
+        criterion = torch.nn.CrossEntropyLoss()
+    
     model.train()
     total_loss = 0
     total_correct = 0
     total_count = 0
 
     for step, batch in enumerate(
-        tqdm(loader, desc=f"Epoch {epoch}", leave=False)
+        tqdm(loader, desc=f"Epoch {epoch+1}", leave=False)
     ):
         batch = {k: v.to(device, non_blocking=True) for k, v in batch.items()}
 
-        out = model(**batch)
-        loss = out.loss
+        # print(torch.argmax(out['logits'], dim = 1))
+        out = model(
+            input_ids=batch["input_ids"],
+            attention_mask=batch["attention_mask"]
+        )
+        logits = out.logits
+        labels = batch["labels"].long()
+
+        loss = criterion(logits, labels)
 
         optimizer.zero_grad()
         loss.backward()
@@ -48,7 +58,9 @@ def train_one_epoch(model, loader, optimizer, scheduler, device, epoch, writer, 
 
 
 @torch.no_grad()
-def evaluate(model, loader, device, epoch, writer):
+def evaluate(model, loader, device, epoch, writer, criterion = None):
+    if criterion is None:
+        criterion = torch.nn.CrossEntropyLoss()
     model.eval()
     total_loss = 0
     total_correct = 0
@@ -56,12 +68,25 @@ def evaluate(model, loader, device, epoch, writer):
 
     for batch in tqdm(loader, desc="Evaluating"):
         batch = {k: v.to(device) for k, v in batch.items()}
-        out = model(**batch)
-        loss = out.loss
+        out = model(
+            input_ids=batch["input_ids"],
+            attention_mask=batch["attention_mask"]
+        )
 
-        preds = out.logits.argmax(dim=-1)
-        labels = batch["labels"]
+        logits = out.logits
+        labels = batch["labels"].long()
 
+        loss = criterion(logits, labels)
+        preds = logits.argmax(dim=-1)
+
+        num_zeros = (preds == 0).sum().item()
+        num_ones  = (preds == 1).sum().item()
+        print(f"0s: {num_zeros}, 1s: {num_ones}")
+
+        num_zeros = (preds == 0).sum().item()
+        num_ones  = (preds == 1).sum().item()
+
+        print(f"0s: {num_zeros}, 1s: {num_ones}")
         total_correct += (preds == labels).sum().item()
         total_count += labels.size(0)
         total_loss += loss.item() * labels.size(0)
